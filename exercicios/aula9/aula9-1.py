0255651199310526ad1ce2a351a25fd08385dc5f
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-02-15 12:58:16 -0300

    commit

---
+++ exercicios/aula9/aula9-1.py
@@ -0,0 +1,8 @@
+"""
+Entrada de dados
+"""
+
+num_1 = int(input('Digite um número: '))
+num_2 = int(input('Digite outro: '))
+
+print(num_1 ** num_2)
---
+++ exercicios/aula9/aula9.py
@@ -0,0 +1,10 @@
+"""
+Entrada de dados
+"""
+
+nome = input("Qual o seu nome? ")
+idade = input("Qual a sua idade? ")
+ano_nasc = 2022 - int(idade)
+print()
+print(f'{nome} tem {idade} anos e nasceu em {ano_nasc}')
+print()
