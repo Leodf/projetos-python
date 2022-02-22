14235de8ce86d1315f36152beb0475a840e4dc9f
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-02-22 11:59:12 -0300

    commit

---
+++ exercicios/aula26/aula26.py
@@ -0,0 +1,8 @@
+"""
+Operador condicional Or
+"""
+
+nome = input('Qual o seu nome? ')
+
+print(nome or 'Você não digitou nada!')
+
---
+++ exercicios/aula27/aula27.py
@@ -0,0 +1,19 @@
+"""
+Desafio do contador com estrutura de repetição
+for / while
+0 10
+1 9
+2 8
+3 7
+4 6
+5 5
+6 4
+7 3
+8 2
+9 1
+10 0
+"""
+
+for n1, n2 in enumerate(range(10, 0, -1)):
+    print(n1, n2)
+    
