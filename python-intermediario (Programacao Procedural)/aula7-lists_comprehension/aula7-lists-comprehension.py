9028d61b4401dfbe2f1cb98c47a14cfc120ee371
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-03-08 10:16:32 -0300

    commit

--- python-intermediario (Programacao Procedural)/aula7-lists_comprehension/aula7-lists-comprehension.py
+++ python-intermediario (Programacao Procedural)/aula7-lists_comprehension/aula7-lists-comprehension.py
@@ -8,6 +8,7 @@ ex1 = [variavel for variavel in l1]
 
 ex2 = [v*2 for v in l1]
 ex3 = [(v, v2) for v in l1 for v2 in range(3)]
+print(ex3)
 
 l2 = ['Leonardo', 'Luiz', 'Mauro', 'Maria']
 ex4 = [v.replace('a', '@').upper() for v in l2]
@@ -24,4 +25,3 @@ l3 = list(range(100))
 ex6 = [v for v in l3 if v % 3 == 0 if v % 8 == 0]
 
 ex7 = [v if v % 3 == 0 else 'Não é' for v in l3]
-print(ex7)
---
+++ python-intermediario (Programacao Procedural)/aula9-geradores_iteradores_iteraveis/aula9-exercicio-list_comprehension.py
@@ -0,0 +1,11 @@
+
+
+carrinho = [
+    ('Produto1', 10),
+    ('Produto2', 20),
+    ('Produto3', 30),
+]
+
+total = sum([float(y) for x, y in carrinho])
+print(carrinho)
+print(total)
