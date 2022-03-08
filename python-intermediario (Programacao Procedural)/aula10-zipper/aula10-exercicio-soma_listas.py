1d896c7ac2b8bb5f4084186725810948d5745057
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-03-08 11:02:07 -0300

    Update aula10-exercicio-soma_listas.py

--- python-intermediario (Programacao Procedural)/aula10-zipper/aula10-exercicio-soma_listas.py
+++ python-intermediario (Programacao Procedural)/aula10-zipper/aula10-exercicio-soma_listas.py
@@ -10,6 +10,27 @@ lista_b = [1,2,3,4]
 =========== resultado
 lista_soma = [2,4,6,8]
 """
+"""
+solução genérica
+lista_a = [1,2,3,4,5,6,7]
+lista_b = [1,2,3,4]
+
+lista_soma = []
+for i in range(len(lista_b)):
+    lista_soma.append(lista_a[i] + lista_b[i])
+print(lista_soma)
+"""
+"""
+lista_a = [1,2,3,4,5,6,7]
+lista_b = [1,2,3,4]
+
+lista_soma = []
+for i, _ in enumerate(lista_b):
+    lista_soma.append(lista_a[i] + lista_b[i])
+print(lista_soma)
+"""
+"""
+minha solução
 lista_a = [1,2,3,4,5,6,7]
 lista_b = [1,2,3,4,5,6,7,8,9]
 
@@ -20,8 +41,11 @@ for lista_a, lista_b in lista_zip:
     lista_soma.append(lista_a + lista_b)
     
 print(lista_soma)
-
-
+"""
+lista_a = [1,2,3,4,5,6,7]
+lista_b = [1,2,3,4,5,6,7,8,9]
+lista_soma = [x + y for x, y in zip(lista_a, lista_b)]
+print(lista_soma)
 
 
 
