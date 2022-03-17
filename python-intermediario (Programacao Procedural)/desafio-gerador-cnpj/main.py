ea1a8ee8d6e04fe7f37476f45b4654c87df2af27
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-03-17 17:56:25 -0300

    commit

--- python-intermediario (Programacao Procedural)/desafio-gerador-cnpj/main.py
+++ python-intermediario (Programacao Procedural)/desafio-gerador-cnpj/main.py
@@ -6,3 +6,8 @@ if cnpj.valida(cnpj1):
     print(f'{cnpj1} é válido')
 else:
     print(f'{cnpj1} é inválido')
+
+for i in range(100):
+    novo_cnpj = cnpj.gera()
+    formatado = cnpj.formata(novo_cnpj)
+    print(formatado)
