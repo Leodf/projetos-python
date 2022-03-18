1ab917d9e6e83615730862a090a9ad302462e487
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-03-18 15:36:24 -0300

    commit

---
+++ python-poo (programacao orientada a objeto)/aula7-associacao/associacao.py
@@ -0,0 +1,13 @@
+
+from classes import Escritor, Caneta, MaquinaDeEscrever
+
+escritor = Escritor('Joãozinho')
+caneta = Caneta('Bic')
+maquina = MaquinaDeEscrever()
+
+escritor.ferramenta = maquina
+escritor.ferramenta.escrever()
+
+del escritor
+print(caneta.marca)
+maquina.escrever()
