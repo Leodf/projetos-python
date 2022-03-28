871daeecce8501c4ca11f098b8395967a2a307a1
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-03-28 11:55:13 -0300

    Update main.py

--- python-poo (programacao orientada a objeto)/aula21-dataclasses/main.py
+++ python-poo (programacao orientada a objeto)/aula21-dataclasses/main.py
@@ -12,6 +12,11 @@ class Pessoa:
     sobrenome: str
 
     def __post_init__(self):
+        if not isinstance(self.nome, str):
+            raise TypeError(
+                f'Tipo inválido {type(self.nome).__name__} != str em {self}'
+            )
+
         self.nome_completo = f'{self.nome} {self.sobrenome}'
 
     # @property
