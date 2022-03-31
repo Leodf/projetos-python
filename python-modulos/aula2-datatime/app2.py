6feb39ac71399721974725b8d1fa60227a95e847
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-03-31 17:22:05 -0300

    Update app2.py

--- python-modulos/aula2-datatime/app2.py
+++ python-modulos/aula2-datatime/app2.py
@@ -4,5 +4,7 @@ from locale import setlocale, LC_ALL
 setlocale(LC_ALL, 'pt_BR.utf-8')
 
 dt = datetime.now()
-formatacao = dt.strftime('%A, %d de %B de %Y')
-print(formatacao)
+formatacao1 = dt.strftime('%A, %d de %B de %Y')
+formatacao2 = dt.strftime('%d/%m/%Y %H:%M:%S')
+print(formatacao1)
+print(formatacao2)
