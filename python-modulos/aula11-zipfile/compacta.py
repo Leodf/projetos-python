0afeb17e40f91b606220b3bd5b7316c615893de7
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-04-12 08:49:10 -0300

    Update compacta.py

--- python-modulos/aula11-zipfile/compacta.py
+++ python-modulos/aula11-zipfile/compacta.py
@@ -11,3 +11,6 @@ with ZipFile('arquivo.zip', 'w') as zip:
 with ZipFile('arquivo.zip', 'r') as zip:
     for arquivo in zip.namelist():
         print(arquivo)
+
+with ZipFile('arquivo.zip', 'r') as zip:
+    zip.extractall('descompactado')
