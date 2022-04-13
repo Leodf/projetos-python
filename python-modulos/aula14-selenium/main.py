cfc2ecac6d1174740a602c8dcaf3c7f86bccac96
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-04-13 18:58:44 -0300

    commmit

--- python-modulos/aula14-selenium/main.py
+++ python-modulos/aula14-selenium/main.py
@@ -1,1 +1,24 @@
 
+from selenium import webdriver
+from time import sleep
+
+class ChromeAuto:
+    def __init__(self):
+        self.driver_path = 'chromedriver'
+        self.options = webdriver.ChromeOptions()
+        self.options.add_argument('user-data-dir=Perfil')
+        self.chrome = webdriver.Chrome(
+            self.driver_path,
+            options=self.options
+        )
+
+    def acessa(self, site):
+        self.chrome.get(site)
+
+    def sair(self):
+        self.chrome.quit()
+
+if __name__ == '__main__':
+    chrome = ChromeAuto()
+    chrome.acessa('https://github.com/')
+    
