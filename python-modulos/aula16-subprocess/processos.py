34e89c51ace87ff49409ce3ddef02ded036493d1
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-04-14 09:33:07 -0300

    Create processos.py

---
+++ python-modulos/aula16-subprocess/processos.py
@@ -0,0 +1,20 @@
+import subprocess
+from typing import Text
+
+# Windows - ping 127.0.0.1
+# Linux - ping 127.0.0.1 -c 4
+
+proc = subprocess.run(
+    ['ping', '127.0.0.1'],
+    capture_output=True,
+    text = True,
+)
+
+# print(proc.stderr)
+# print(proc.stdout)
+# print(proc.returncode)
+# print(proc.args)
+
+saida = proc.stdout
+saida = saida.replace('bytes', 'Leonardo')
+print(saida)
