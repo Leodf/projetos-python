bd43362188c6f2d033c0de3d2e8db3a4bf98fd11
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-04-23 10:15:17 -0300

    commit

--- python-modulos/aula19-pypdf2/main.py
+++ python-modulos/aula19-pypdf2/main.py
@@ -7,8 +7,8 @@ https://www.blog.pythonlibrary.org/2018/06/07/an-intro-to-pypdf2/
 import PyPDF2
 import os
 
+"""
 caminho_dos_pdfs = r'.\python-modulos\aula19-pypdf2\pdf'
-
 
 novo_pdf = PyPDF2.PdfFileMerger()
 for root, dirs, files in os.walk(caminho_dos_pdfs):
@@ -20,3 +20,16 @@ for root, dirs, files in os.walk(caminho_dos_pdfs):
 
 with open(f'{caminho_dos_pdfs}/novo_arquivo.pdf', 'wb') as meu_novo_pdf:
     novo_pdf.write(meu_novo_pdf)
+"""
+
+with open('./python-modulos/aula19-pypdf2/pdf/novo_arquivo.pdf', 'rb') as arquivo_pdf:
+    leitor = PyPDF2.PdfFileReader(arquivo_pdf)
+    num_paginas = leitor.getNumPages()
+    
+    for num_pagina in range(num_paginas):
+        escritor = PyPDF2.PdfFileWriter()
+        pagina_atual = leitor.getPage(num_pagina)
+        escritor.addPage(pagina_atual)
+
+        with open(f'./python-modulos/aula19-pypdf2/novos_pdfs/{num_pagina}.pdf', 'wb') as novo_pdf:
+            escritor.write(novo_pdf)
