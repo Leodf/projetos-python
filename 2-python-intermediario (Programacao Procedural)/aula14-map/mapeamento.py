3cf47989e54f4a40ce2542efb9e749131b52bd19
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-04-26 11:42:49 -0300

    commit

--- 2-python-intermediario (Programacao Procedural)/aula14-map/mapeamento.py
+++ 2-python-intermediario (Programacao Procedural)/aula14-map/mapeamento.py
@@ -6,7 +6,7 @@ from dados import produtos, pessoas, lista
 # print(lista)
 # print(list(nova_lista))
 
-"""
+
 def aumenta_preco(p):
     p['preco'] = round(p['preco'] * 1.05, 2)
     return p
@@ -15,6 +15,7 @@ novos_produtos = map(aumenta_preco, produtos)
 
 for produto in novos_produtos:
     print(produto)
+
 """
 def aumenta_idade(p):
     p['nova_idade'] = round(p['idade'] * 1.20)
@@ -24,3 +25,4 @@ nomes = map(aumenta_idade, pessoas)
 
 for pessoa in nomes:
     print(pessoa)
+"""
