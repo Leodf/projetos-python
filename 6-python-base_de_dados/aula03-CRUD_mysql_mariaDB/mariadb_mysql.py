2014479499e2874f36a4fab7e4e5617ca4a136d2
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-04-27 18:48:15 -0300

    Update mariadb_mysql.py

--- 6-python-base_de_dados/aula03-CRUD_mysql_mariaDB/mariadb_mysql.py
+++ 6-python-base_de_dados/aula03-CRUD_mysql_mariaDB/mariadb_mysql.py
@@ -1,19 +1,78 @@
 import pymysql.cursors
+from contextlib import contextmanager
 
-conexao = pymysql.connect(
-    host='127.0.0.1',
-    user='root',
-    password='',
-    db='clientes',
-    charset='utf8mb4',
-    cursorclass=pymysql.cursors.DictCursor
-)
+# CRUD - CREATE, READ, UPDATE, DELETE
 
-with conexao.cursor() as cursor:
-    cursor.execute('SELECT * FROM clientes')
-    resultado = cursor.fetchall()
+@contextmanager
+def conecta():
+    conexao = pymysql.connect(
+        host='127.0.0.1',
+        user='root',
+        password='',
+        db='clientes',
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor
+    )
 
-    print(resultado)
+    try:
+        yield conexao
+    finally:
+        print('conexão fechada')
+        conexao.close()
 
+# # Inserindo 1 registro na base de dados
+# with conecta() as conexao:
+#     with conexao.cursor() as cursor:
+#         sql = 'INSERT INTO clientes (nome, sobrenome, idade, peso) VALUES (%s, %s, %s, %s)'
+#         cursor.execute(sql, ('Jack', 'Monroe', 112, 220))
+#         conexao.commit()
 
-conexao.close()
+# #Inserindo varios registros na base de dados
+# with conecta() as conexao:
+#     with conexao.cursor() as cursor:
+#         sql = 'INSERT INTO clientes (nome, sobrenome, idade, peso) VALUES (%s, %s, %s, %s)'
+
+#         dados = [
+#             ('Odair', 'Junior', 45, 95),
+#             ('Rose', 'Otávio', 25, 65),
+#             ('João', 'Doria', 43, 70),
+#         ]
+#         cursor.executemany(sql, dados)
+#         conexao.commit()
+
+# # deletando 1 valor
+# with conecta() as conexao:
+#     with conexao.cursor() as cursor:
+#         sql = 'DELETE FROM clientes WHERE id = %s'
+#         cursor.execute(sql, (7,))
+#         conexao.commit()
+
+# # deletando varios registros determinados
+# with conecta() as conexao:
+#     with conexao.cursor() as cursor:
+#         sql = 'DELETE FROM clientes WHERE id IN (%s, %s, %s)'
+#         cursor.execute(sql, (8, 9, 10))
+#         conexao.commit()
+
+# # deletando varios valor de um intervalo
+# with conecta() as conexao:
+#     with conexao.cursor() as cursor:
+#         sql = 'DELETE FROM clientes WHERE id BETWEEN %s AND %s'
+#         cursor.execute(sql, (5, 13))
+#         conexao.commit()
+
+# # atualizando  valor
+# with conecta() as conexao:
+#     with conexao.cursor() as cursor:
+#         sql = 'UPDATE clientes SET nome = %s WHERE id=%s'
+#         cursor.execute(sql, ('Silvia', 4))
+#         conexao.commit()
+
+# Este seleciona os dados da base de dados
+with conecta() as conexao:
+    with conexao.cursor() as cursor:
+        cursor.execute('SELECT * FROM clientes LIMIT 100')
+        resultado = cursor.fetchall()
+
+        for linha in resultado:
+            print(linha)
