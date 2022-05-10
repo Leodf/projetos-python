2b494c02841d4dd8b00090986879c2aa19eca2af
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-05-10 13:09:11 -0300

    commit

--- 7-python-django_basico/aula02-agenda/agenda/settings.py
+++ 7-python-django_basico/aula02-agenda/agenda/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'contatos.apps.ContatosConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -105,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -128,3 +129,14 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Mensagens
+from django.contrib.messages import constants
+
+MESSAGE_TAGS = {
+    constants.ERROR: 'alert-danger',
+    constants.WARNING: 'alert-warning',
+    constants.DEBUG: 'alert-info',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
+}
--- 7-python-django_basico/aula02-agenda/contatos/views.py
+++ 7-python-django_basico/aula02-agenda/contatos/views.py
@@ -1,12 +1,16 @@
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from . models import Contato
 from django.core.paginator import Paginator
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
+from django.contrib import messages
 
 def index(request):
     contatos = Contato.objects.order_by('id').filter(
         mostrar=True
     )
+
     paginator = Paginator(contatos, 5)
 
     page = request.GET.get('p')
@@ -24,4 +28,32 @@ def ver_contato(request, contato_id):
 
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
+    })
+
+def busca(request):
+    termo = request.GET.get('termo')
+
+    if termo is None or not termo:
+        messages.add_message(
+            request,
+            messages.ERROR,
+            'Campo não pode ficar vazio.'
+        )
+        return redirect('index')
+
+    campos = Concat('nome', Value(' '), 'sobrenome')
+
+    contatos = Contato.objects.annotate(
+        nome_completo=campos
+    ).filter(
+        Q(nome_completo__icontains=termo) | Q(telefone__icontains=termo)
+    )
+
+    paginator = Paginator(contatos, 5)
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
+    return render(request, 'contatos/busca.html', {
+        'contatos': contatos
     })
